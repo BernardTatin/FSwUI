4a81c7775770106d28c5fc00251d93658e9c06b3
Bernard Tatin <btat1.ernard@tutanota.com>
2022-08-06 23:17:13 +0200

    .gitignore

---
+++ D1/Program.fs
@@ -0,0 +1,27 @@
+﻿(*
+ * D1/Program.fs
+ * see:
+ *      https://stackoverflow.com/questions/50373607/how-can-i-enable-a-winforms-or-wpf-project-in-f
+ *
+ * Steps:
+ *  - Create a new console application in F#,
+ *  - Select the solution properties,
+ *      - change "Output type" in the "Application" page from "Console application" to "Windows Application"
+ *  - Right click on "References" in the project
+ *      - add reference to "System.Windows.Forms" and "System.Drawing" (in the "Framework" tab).
+ *
+ * Etapes
+ *  - Créer une application console en f#,
+ *  - Afficher les propriétés de la solutions
+ *      - changer "Type de Sortie" de "Application Console" en "Application Windows"
+ *  - Click droit sur les "Dépendances" de la solution,
+ *      - ajouter les  "référence d'assembly" -> "Framework":
+ *          - "System.Windows.Forms"
+ *          - "System.Drawing"
+ *)
+open System.Windows.Forms
+
+let f = new Form(Width= 400, Height = 300, Visible = true, Text = "Hello World")
+f.Controls.Add(new Label(Text="Hello world!"))
+Application.Run(f)
+

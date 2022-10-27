6c47d0dde162eb0911d2c9b7bab90ae06a771dd9
Bernard Tatin <bernard.tatin@outlook.fr>
2022-10-27 11:15:49 +0200

    Compilation configuration

--- FImage/run.bat
+++ FImage/run.bat
@@ -1,6 +1,14 @@
-dotnet clean -c Release
-REM dotnet build -c Release -p:DefineConstants=LOGGER
-REM dotnet build -c Release -p:DefineConstants=RECURSEBM -p:DefineConstants=LOGGER
-dotnet build -c Release -p:DefineConstants=\"LOGGER;RECURSEBM"
-REM dotnet build -c Release
-bin\Release\net48\FImage.exe
+set release=Release4Perf
+if %release%==Release4Perf (
+    set options=-p:DefineConstants=\"LOGGER;RECURSEBM"
+) else (
+    set options=""
+)
+dotnet clean -c %release%
+REM || goto :ERROR
+dotnet build -c %release% %options% || goto :ERROR
+bin\%release%\net48\FImage.exe || goto :ERROR
+goto :END
+:ERROR
+echo "Cannot compile and run %release%"
+:END
